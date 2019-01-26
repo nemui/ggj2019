413b80cc815273ad7563ee8b5baa85ef9f8182f1
Dmitry Mamchur <metaphysical.intoxication@gmail.com>
2019-01-26 18:13:33 +0000

    add intro

---
+++ Project Files/Assets/Narrator.cs
@@ -0,0 +1,19 @@
+﻿using Fungus;
+using UnityEngine;
+
+public class Narrator : MonoBehaviour
+{
+    public Flowchart flowchart;
+
+    public int systemCount;
+    public int wakedSystems;
+
+    public void WakedASystem()
+    { 
+        if ( ++wakedSystems == systemCount )
+        {
+            flowchart.SetBooleanVariable("WakedAllSystems", true);
+        }
+
+    }
+}
