33caf515c6f65afc35b28e902b38527ec7afc9fb
Paul <paulfahey1996@gmail.com>
2019-01-26 09:47:38 +0000

    Combat testing

---
+++ Project Files/Assets/Scripts/CombatTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatTest : MonoBehaviour
+{
+    public List<string> inputString = new List<string>();
+
+    void OnGUI()
+    {
+        if (Input.anyKeyDown)
+        {
+
+            if (Event.current.isKey)
+            {
+                Event e = Event.current;
+
+                if (e.isKey)
+                {
+                    Debug.Log("Detected key code: " + e.keyCode);
+                    string eString = e.keyCode.ToString();
+                    if(eString != "None")
+                    {
+                        inputString.Add(eString);
+                    }
+                }
+
+            }
+        }
+    }
+
+    private void Update()
+    {
+
+        
+    }
+}
