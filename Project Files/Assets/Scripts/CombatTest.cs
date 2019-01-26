8a2977dc75c57af32f822c4964884fa150994ef9
Paul <paulfahey1996@gmail.com>
2019-01-26 12:41:53 +0000

    Player input

--- Project Files/Assets/Scripts/CombatTest.cs
+++ Project Files/Assets/Scripts/CombatTest.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CombatTest : MonoBehaviour
 {
-    public List<string> inputString = new List<string>();
+    public List<InputKey> inputList = new List<InputKey>();
 
     void OnGUI()
     {
@@ -19,9 +19,12 @@ public class CombatTest : MonoBehaviour
                 {
                     Debug.Log("Detected key code: " + e.keyCode);
                     string eString = e.keyCode.ToString();
-                    if(eString != "None")
+                    if (eString != "None")
                     {
-                        inputString.Add(eString);
+                        InputKey thisKey = new InputKey();
+                        thisKey.keyCode = eString;
+                      
+                        RunLoop(thisKey);
                     }
                 }
 
@@ -29,9 +32,69 @@ public class CombatTest : MonoBehaviour
         }
     }
 
-    private void Update()
+    private void Start()
     {
+        StartCoroutine(Decay());
+    }
 
-        
+    private void RunLoop(InputKey key)
+    {
+       bool keyInList = false;
+
+       foreach(InputKey inputKey in inputList)
+        {
+            if (inputKey.keyCode == key.keyCode)
+            {
+                inputKey.timesPressed++;
+                Debug.Log("Iterate" + inputKey.keyCode + inputKey.timesPressed);
+                keyInList = true;
+            }
+        }
+
+        if (!keyInList)
+        {
+            // Add key to list.
+            Debug.Log("Not in list. Adding");
+            inputList.Add(key);
+            key.timesPressed++;
+            Debug.Log(key.keyCode + key.timesPressed);
+        }
+    }
+
+    private IEnumerator Decay()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(2);
+
+            Debug.Log("Starting decay");
+            if (inputList.Count > 0)
+            {
+                foreach (InputKey inputKey in inputList)
+                {
+                    inputKey.timesPressed--;
+                    Debug.Log("Decreased " + inputKey.keyCode + " to " + inputKey.timesPressed);
+                    
+                }
+                foreach (InputKey key2 in inputList)
+                {
+                    if (key2.timesPressed <= 0)
+                    {
+                        inputList.Remove(key2);
+                        Debug.Log("Removed" + key2.keyCode);
+                        break;
+                    }
+                }
+
+            }
+        }
     }
 }
+
+public class InputKey
+{
+    public string keyCode;
+    public int timesPressed;
+    public bool isEntered;
+}
+
