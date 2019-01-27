96d5768c04900e6213c2b0bd35123ad595084140
Dmitry Mamchur <metaphysical.intoxication@gmail.com>
2019-01-27 12:04:43 +0000

    narrative art, some portraits

--- Project Files/Assets/Narrator.cs
+++ Project Files/Assets/Narrator.cs
@@ -13,6 +13,7 @@ public class Narrator : MonoBehaviour
     {
         introFlowchart = GameObject.Find("IntroFlowchart").GetComponent<Flowchart>();
         talkFlowchart = GameObject.Find("TalksFlowchart").GetComponent<Flowchart>();
+        //infoDump.firstTime = true;
         if (infoDump.firstTime)
         {
             introFlowchart.enabled = true;
