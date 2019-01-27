c1f0cb83f56f8f7f942d165d0b8e300aee2163ae
Dmitry Mamchur <metaphysical.intoxication@gmail.com>
2019-01-27 15:49:32 +0000

    dirty hack

---
+++ Project Files/Assets/ScriptableObjectHolder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptableObjectHolder : MonoBehaviour
+{
+    public InfoDump infoDump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
